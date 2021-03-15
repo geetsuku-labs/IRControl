50966d1389aba2611950f47c174bc9af58f68339
qst0528 <quantum.suto@gmail.com>
2021-03-16 00:46:49 +0900

    Add more IR codes

--- include/config.hpp
+++ include/config.hpp
@@ -2,6 +2,7 @@
 #define CONFIG_HPP
 
 #include <Arduino.h>
+#include <IRremoteInt.h>
 #include "credentials.hpp"
 
 namespace config
@@ -10,6 +11,13 @@ namespace config
     {
         const char    *HOSTNAME = "remote";
         const uint16_t OTA_PORT = 8266;
+    };
+
+    namespace ir
+    {
+              IRData   YAMAHA_POWER   = { NEC, 0x7EU, 0x2AU, 0x0, 0x0, IRDATA_FLAGS_EMPTY, 0x0, NULL};
+              IRData   YAMAHA_VOL_INC = { NEC, 0x7AU, 0x1AU, 0x0, 0x0, IRDATA_FLAGS_EMPTY, 0x0, NULL};
+              IRData   YAMAHA_VOL_DEC = { NEC, 0x7AU, 0x1BU, 0x0, 0x0, IRDATA_FLAGS_EMPTY, 0x0, NULL};
     };
 };
 
