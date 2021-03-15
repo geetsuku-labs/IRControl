f64130e6e5c141746861a4c5e597b7b1eaf3ad01
qst0528 <quantum.suto@gmail.com>
2021-03-15 21:29:03 +0900

    Test hardware if it can send real IR code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include <Arduino.h>
+#include <ArduinoLog.h>
+#include <IRremote.h>
 
 void setup() {
+  Serial.begin(115200);
+  Log.begin(LOG_LEVEL_NOTICE, &Serial);
   pinMode(23, OUTPUT_OPEN_DRAIN);
+  IrSender.setSendPin(23);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(23, HIGH);
-  delay(500);
-  digitalWrite(23, LOW);
-  delay(500);
+  IrSender.sendNECMSB(0x7E8154AB, 32, false);
+  delay(5000);
 }
