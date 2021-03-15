f0411c89af66c8bb81eafa605ddbe546269309a6
qst0528 <quantum.suto@gmail.com>
2021-03-15 15:17:39 +0900

    Add minimal code for Hardware test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(23, OUTPUT_OPEN_DRAIN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(23, HIGH);
+  delay(500);
+  digitalWrite(23, LOW);
+  delay(500);
 }
