a43c0ea2b66da5920a2d81de4853e3409d3b22de
qst0528 <quantum.suto@gmail.com>
2021-07-11 12:06:00 +0900

    Update namespace structure

--- include/credentials_example.hpp
+++ include/credentials_example.hpp
@@ -1,8 +1,11 @@
-namespace config
+namespace MODULE
 {
-    namespace wifi
+    namespace IP
     {
-        const char *SSID       = "myESSID";
-        const char *PASSPHRASE = "verySecurePassphrase";
-    }; // namespace wifi
-}; // namespace config
+        namespace WIFI
+        {
+            const char *SSID       = "myESSID";
+            const char *PASSPHRASE = "verySecurePassphrase";
+        }; // namespace wifi
+    }; // namespace ip
+}; // namespace module
