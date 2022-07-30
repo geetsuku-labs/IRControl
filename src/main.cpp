d25adbd3a095eeea28c7a35fed299f22b2c5cd5a
Suto, Masanori <suto@geek-masa.com>
2022-07-31 01:57:11 +0900

    Apply new configuration structure

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,18 @@
 #include <ArduinoLog.h>
 #include <IRremote.h>
 #include "config.hpp"
-
-namespace ir = config::ir;
+#include "IRCodes.hpp"
 
 void setup() {
   Serial.begin(115200);
   Log.begin(LOG_LEVEL_NOTICE, &Serial);
   pinMode(23, OUTPUT_OPEN_DRAIN);
-  IrSender.setSendPin(23);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  IrSender.write(&ir::YAMAHA_VOL_INC);
+  IrSender.write(&IR::YAMAHA_VOL_INC);
   delay(500);
-  IrSender.write(&ir::YAMAHA_VOL_DEC);
+  IrSender.write(&IR::YAMAHA_VOL_DEC);
   delay(500);
 }
