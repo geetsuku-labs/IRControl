1a73866df755921aa608b6036953165f5a6b2636
Suto, Masanori <suto@geek-masa.com>
2022-07-31 02:30:16 +0900

    Fix indent

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,22 @@
 #include "IRCodes.hpp"
 
 namespace {
-  void serialSetup(HardwareSerial &serial, unsigned long baud) {
-    serial.begin(baud);
-    serial.println(F("Booting PVE Control..."));
-  }
+    void serialSetup(HardwareSerial &serial, unsigned long baud) {
+        serial.begin(baud);
+        serial.println(F("Booting PVE Control..."));
+    }
 } // namespace
 
 void setup() {
-  serialSetup(Serial, 115200);
-  LOG::setup();
-  pinMode(IR_SEND_PIN, OUTPUT_OPEN_DRAIN);
+    serialSetup(Serial, 115200);
+    LOG::setup();
+    pinMode(IR_SEND_PIN, OUTPUT_OPEN_DRAIN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  IrSender.write(&IR::YAMAHA_VOL_INC);
-  delay(500);
-  IrSender.write(&IR::YAMAHA_VOL_DEC);
-  delay(500);
+    IrSender.write(&IR::YAMAHA_VOL_INC);
+    delay(500);
+    IrSender.write(&IR::YAMAHA_VOL_DEC);
+    delay(500);
 }
